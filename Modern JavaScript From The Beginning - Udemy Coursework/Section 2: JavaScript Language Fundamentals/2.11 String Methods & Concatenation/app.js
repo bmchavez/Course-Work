78beb66aa8192d7885d602dc427114ee5f151510
Ben Chavez <ben.m.chavez@gmail.com>
2020-06-23 11:46:49 -0500

    string methods & concatenation

---
+++ Modern JavaScript From The Beginning - Udemy Coursework/Section 2: JavaScript Language Fundamentals/2.11 String Methods & Concatenation/app.js
@@ -0,0 +1,64 @@
+const firstName = 'William';
+const lastName = 'Johnson';
+const age = 36;
+const str = 'Hello there my name is Brad';
+const tags = 'web design,web development, programming'
+
+let val;
+
+val = firstName + lastName
+
+// Concatenation
+val = firstName + ' ' + lastName;
+
+// Append
+val = 'Brad ';
+val += 'Traversy';
+
+val = 'Hello, my name is ' + firstName + ' and I am ' + age;
+
+// Escaping
+val = 'That\'s awesome, I can\'t wait';
+
+// Length
+val = firstName.length;
+
+// concat
+val = firstName.concat(' ', lastName);
+
+// Change case
+val = firstName.toUpperCase();
+val = firstName.toLowerCase();
+
+val = firstName[0];
+
+// indexOf();
+val = firstName.indexOf('l');
+val = firstName.lastIndexOf('l');
+
+// charAt()
+val = firstName.charAt('2');
+
+// Get last char
+val = firstName.charAt(firstName.length -1);
+
+// Get substrings
+val = firstName.substring(0, 4);
+
+// slice()
+val = firstName.slice(0, 4);
+val = firstName.slice(-3); // last three characters
+
+// split()
+val = str.split(' ');
+val = tags.split(',');
+
+// replace()
+val = str.replace('Brad', 'Jack');
+
+// includes()
+val = str.includes('Hello'); // Test to see if a substring exists
+
+
+
+console.log(val);
