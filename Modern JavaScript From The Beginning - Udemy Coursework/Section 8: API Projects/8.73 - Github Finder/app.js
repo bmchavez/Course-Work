61958e166e4255d0a2c53a983818c9e6db1fe84d
Ben Chavez <ben.m.chavez@gmail.com>
2020-07-06 14:53:38 -0500

    fetching profile data

---
+++ Modern JavaScript From The Beginning - Udemy Coursework/Section 8: API Projects/8.73 - Github Finder/app.js
@@ -0,0 +1,26 @@
+// Init Github
+const github = new GitHub;
+
+// Search input
+const searchUser = document.getElementById('searchUser');
+
+// Search input event listener
+searchUser.addEventListener('keyup', (e) => {
+  // Get input text
+  const userText = e.target.value;
+
+  if(userText !== ''){
+    console.log(userText);
+    // Make http call 
+    github.getUser(userText)
+    .then(data => {
+      if(data.profile.message === 'Not Found') {
+        // Show alert
+
+      } else {
+        // Show profile
+        
+      }
+    })
+  }
+});
