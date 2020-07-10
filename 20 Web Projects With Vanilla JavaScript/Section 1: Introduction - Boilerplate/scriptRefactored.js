2378d01ff6210640cd5625ba31043196ab694760
Ben Chavez <ben.m.chavez@gmail.com>
2020-07-09 21:54:34 -0500

    pre-refactor

---
+++ 20 Web Projects With Vanilla JavaScript/Section 1: Introduction - Boilerplate/scriptRefactored.js
@@ -0,0 +1,50 @@
+// Get Dom Elements
+const form = document.getElementById('form');
+const username = document.getElementById('username');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+const pasword2 = document.getElementById('password2');
+// const submit = document.querySelector("button").addEventListener('click', formSubmit);
+
+// FUNCTIONS
+// Show input error message
+function showError(input, message) {
+  const formControl = input.parentElement
+  // formControl.classList.add('error');
+  formControl.className = 'form-control error';
+
+  const small = formControl.querySelector('small');
+  small.innerText = message;
+}
+
+// Show success outline
+function showSuccess(input) {
+  const formControl = input.parentElement
+  // formControl.classList.add('error');
+  formControl.className = 'form-control success';
+}
+
+// Check if email is valid
+function isValidEmail(email) {
+  const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(String(email).toLowerCase());
+}
+
+// Check required fields
+function checkRequired(inputArr) {
+  inputArr.forEach();
+}
+
+// EVENT LISTENERS
+// Add event listener to submit action
+form.addEventListener('submit', function(e) {
+  e.preventDefault();
+
+  checkRequired([username, email, password, password2]);
+});
+
+
+
+
+
+
