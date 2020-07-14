597f61d069b90e8e84c4c86f03d875cf67a8f64b
Ben Chavez <ben.m.chavez@gmail.com>
2020-07-14 16:22:19 -0500

    Get posts from rest api and display

---
+++ Modern JavaScript From The Beginning - Udemy Coursework/Section 13: Microposts Project - CRUD Front End (Webpack & Babel)/src/app.js
@@ -0,0 +1,12 @@
+// npm install json-server --save
+import { http } from './http';
+import { ui } from './ui';
+
+// Get posts on Dom load
+document.addEventListener('DOMContentLoaded', getPosts);
+
+function getPosts() {
+  http.get('http://localhost:3000/posts')
+    .then(data => ui.showPosts(data))
+    .catch(err => console.log(err));
+}
