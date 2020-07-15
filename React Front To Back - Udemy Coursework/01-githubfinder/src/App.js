08092633f0bbcbcf4af2e5cc73ec82172c4ff936
Ben Chavez <ben.m.chavez@gmail.com>
2020-07-15 15:43:49 -0500

    components, props, and proptypes

--- React Front To Back - Udemy Coursework/01-githubfinder/src/App.js
+++ React Front To Back - Udemy Coursework/01-githubfinder/src/App.js
@@ -1,25 +1,16 @@
 import React, { Fragement, Component } from 'react';
+import Navbar from './components/layout/Navbar'
 import './App.css';
 
 // Class based component rather than functional
 class App extends Component {
-  // foo = () => 'Bars'
-
-
   render() {
-    const name = 'John Doe';
-    const loading = false;
-    const showName = false;
-
-    // if(loading) {
-    //   return <h4>Loading...</h4>
-    // }
 
     return (
       // JSX JavaScript Syntax Extension - (not HTML) 
-      <div className="App">
-        {loading ? <h4>Loading...</h4> : <h1>Hello {showName && name} From React</h1>}
-      </div>
+      <nav className="navbar bg-primary">
+        <Navbar />
+      </nav>
     );
   }
 }
