824170f99bda16f5a1b0a1a693b8e632135c2baa
Ben Chavez <ben.m.chavez@gmail.com>
2020-07-15 14:18:56 -0500

    file cleanup

--- 20 Web Projects With Vanilla JavaScript/Section 2: Form Validator | Intro Project/scriptRefactored.js
+++ 20 Web Projects With Vanilla JavaScript/Section 2: Form Validator | Intro Project/scriptRefactored.js
@@ -24,16 +24,6 @@ function showSuccess(input) {
   formControl.className = 'form-control success';
 }
 
-// Check if email is valid
-function checkEmail(input) {
-  const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  if(re.test(input.value.trim())) {
-    showSuccess(input);
-  } else {
-    showError(input, 'Email is not valid')
-  }
-}
-
 // Check required fields
 function checkRequired(inputArr) {
   inputArr.forEach(function(input) {
@@ -43,6 +33,16 @@ function checkRequired(inputArr) {
       showSuccess(input);
     }
   });
+}
+
+// Check if email is valid
+function checkEmail(input) {
+  const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  if(re.test(input.value.trim())) {
+    showSuccess(input);
+  } else {
+    showError(input, 'Email is not valid')
+  }
 }
 
 // Check input Length
@@ -56,7 +56,7 @@ function checkLength(input, min, max) {
   }
 }
 
-// Check password maths
+// Check passwords match
 function checkPasswordsMatch(input1, input2) {
   if(input1.value !== input2.value) {
     showError(input2, 'Passwords do not match')
