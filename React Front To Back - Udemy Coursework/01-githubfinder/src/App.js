71ded114437c269de5af927df336024e99733363
Ben Chavez <ben.m.chavez@gmail.com>
2020-07-15 15:01:24 -0500

    fragment and jsx

--- React Front To Back - Udemy Coursework/01-githubfinder/src/App.js
+++ React Front To Back - Udemy Coursework/01-githubfinder/src/App.js
@@ -1,15 +1,17 @@
-import React, { Component } from 'react';
+import React, { Fragement, Component } from 'react';
 import './App.css';
 
 // Class based component rather than functional
 class App extends Component {
   
   render() {
-    return (
-      <div className="App">
-        <h1>Hello From React</h1>
-      </div>
-    );
+    // return (
+    //   // JSX JavaScript Syntax Extension - (not HTML) 
+    //   <div className="App">
+    //     <h1>Hello From React</h1>
+    //   </div>
+    // );
+    return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'hello from react'));
   }
 }
 
