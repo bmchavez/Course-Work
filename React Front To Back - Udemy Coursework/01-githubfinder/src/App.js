95f149acf3b62d6a74f97475d4d39a3ceac1db6f
Ben Chavez <ben.m.chavez@gmail.com>
2020-07-15 15:08:53 -0500

    expressions and conditionals in jsx

--- React Front To Back - Udemy Coursework/01-githubfinder/src/App.js
+++ React Front To Back - Udemy Coursework/01-githubfinder/src/App.js
@@ -3,15 +3,24 @@ import './App.css';
 
 // Class based component rather than functional
 class App extends Component {
-  
+  // foo = () => 'Bars'
+
+
   render() {
-    // return (
-    //   // JSX JavaScript Syntax Extension - (not HTML) 
-    //   <div className="App">
-    //     <h1>Hello From React</h1>
-    //   </div>
-    // );
-    return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'hello from react'));
+    const name = 'John Doe';
+    const loading = false;
+    const showName = false;
+
+    // if(loading) {
+    //   return <h4>Loading...</h4>
+    // }
+
+    return (
+      // JSX JavaScript Syntax Extension - (not HTML) 
+      <div className="App">
+        {loading ? <h4>Loading...</h4> : <h1>Hello {showName && name} From React</h1>}
+      </div>
+    );
   }
 }
 
