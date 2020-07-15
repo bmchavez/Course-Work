b27f035dca0bb55a2aad75c5af6363fd4882ccad
Ben Chavez <ben.m.chavez@gmail.com>
2020-07-15 16:20:52 -0500

    lists & passing state with props

--- React Front To Back - Udemy Coursework/01-githubfinder/src/App.js
+++ React Front To Back - Udemy Coursework/01-githubfinder/src/App.js
@@ -1,16 +1,19 @@
-import React, { Fragement, Component } from 'react';
+import React, { Component } from 'react';
 import Navbar from './components/layout/Navbar'
+import Users from './components/users/Users'
 import './App.css';
 
 // Class based component rather than functional
 class App extends Component {
   render() {
-
     return (
       // JSX JavaScript Syntax Extension - (not HTML) 
-      <nav className="navbar bg-primary">
+      <div className="App">
         <Navbar />
-      </nav>
+        <div className="container">
+          <Users />
+        </div>
+      </div>
     );
   }
 }
